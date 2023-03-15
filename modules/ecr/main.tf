f232de567e3596e70a0de7acf4560ddcabe21ce0
oleksiidn <oleksii.danyliuk@gofore.com>
2023-03-15 12:13:18 +0200

    Add registry for openIMIS REST api images

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -16,6 +16,15 @@ resource "aws_ecr_repository" "ecr_open_imis_gateway" {
   }
 }
 
+resource "aws_ecr_repository" "ecr_open_imis_restapi" {
+  name                 = "open-imis/${var.environment}-restapi"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 resource "aws_ecr_repository" "ecr_mifos_ph_ee_ams" {
   name                 = "payment-hub/${var.environment}-ph-ee-ams"
   image_tag_mutability = "MUTABLE"
