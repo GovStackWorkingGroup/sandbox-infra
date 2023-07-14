240035adad9c5fe6e7d2c93aad72c8a274133eb3
Akseli Karvinen <akseli.karvinen@gofore.com>
2023-07-14 12:13:10 +0300

    moving magic links to portal repository

--- modules/cognitolambda/functions/preSignup/preSignup.ts
+++
@@ -1,6 +0,0 @@
-import { PreSignUpTriggerHandler } from 'aws-lambda'
-
-export const handler: PreSignUpTriggerHandler = async (event) => {
-  event.response.autoConfirmUser = true
-  return event
-}
