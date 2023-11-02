0c8683cd35fecace91650a87dbb10bcc59a79933
tk <tk@tk.com>
2023-11-02 14:58:06 +0200

    fixed type

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -20,7 +20,7 @@ provider "helm" {
   }
 }
 
-provider "" {
+provider "kubectl" {
   apply_retry_count      = 5
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
