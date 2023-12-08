79d92ca2c2f7bcc3e6b6f5d7a682be1e295edef9
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2023-12-08 08:43:28 +0200

    SND-651: Update sandbox cluster

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -2,6 +2,10 @@ output "vpc_id" {
     value = module.vpc.vpc_id
 }
 
+output "vpc_nat_gw_ip" {
+    value = toset(module.vpc.nat_public_ips)
+}
+
 output "sandbox_alb_arn" {
     value = aws_lb.sandbox_alb.arn
 }
