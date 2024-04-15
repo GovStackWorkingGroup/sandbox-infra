7432bf61ea3800a816c907bbb0373c9a3b6fc0eb
oleksiidn <oleksii.danyliuk@gofore.com>
2024-04-15 16:38:31 +0300

    Expose Mifos UI.

--- live/playground/kube/mifos.tf
+++ live/playground/kube/mifos.tf
@@ -1,21 +1,62 @@
-#
-# Target groups
-# TargetGroupBinding resource binds actual endpoints to the tg
-#
-resource "aws_lb_target_group" "mifos-ui" {
-    name = "mifos-ui-tg"
-    port = 80
-    vpc_id = var.vpc_id
-    protocol = "HTTP"
-    target_type = "ip"
+# Exposed services for ID-BB (Mifos)
+resource "aws_lb_target_group" "mifos_bb_ui" {
+  name        = "mifos-bb-ui-tg"
+  port        = 80
+  vpc_id      = var.vpc_id
+  protocol    = "HTTP"
+  target_type = "ip"
 }
 
-locals {
-  backends = {
-    mifos-ui = {
+resource "kubernetes_manifest" "mifos_bb_ui" {
+  lifecycle {
+    replace_triggered_by = [aws_lb_target_group.mifos_bb_ui]
+  }
+  manifest = {
+    apiVersion = "elbv2.k8s.aws/v1beta1"
+    kind       = "TargetGroupBinding"
+    metadata = {
+      name      = aws_lb_target_group.mifos_bb_ui.name
       namespace = "paymenthub"
-      service = "ph-ee-operations-web"
-      tg = aws_lb_target_group.mifos-ui
+    }
+    spec = {
+      serviceRef = {
+        name = "ph-ee-operations-web"
+        port = 80
+      }
+      targetGroupARN = aws_lb_target_group.mifos_bb_ui.arn
+    }
+  }
+}
+
+
+resource "aws_lb_listener_rule" "mifos_bb_admin" {
+  lifecycle {
+    replace_triggered_by = [aws_lb_target_group.mifos_bb_ui]
+  }
+  listener_arn = var.sandbox_alb_listener_arn
+  priority     = 3401
+  tags = {
+    Name = "mifos-bb admin dashboard"
+  }
+
+  action {
+    type             = "authenticate-cognito"
+    authenticate_cognito {
+      user_pool_arn = "${var.user_pool_arn}"
+      user_pool_client_id = "${var.user_pool_client_id}"
+      user_pool_domain = "${var.user_pool_domain}"
+      session_timeout = "7200"
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.mifos_bb_ui.arn
+  }
+
+  condition {
+    host_header {
+      values = ["admin-dashboard.mifos-bb.${var.alb_domain}"]
     }
   }
 }
