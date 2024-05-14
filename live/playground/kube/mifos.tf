bfb5d2fa5df8056e2fc39b30331996d1e07bf5f1
oleksiidn <oleksii.danyliuk@gofore.com>
2024-05-14 21:51:13 +0300

    Expose mifos backend

--- live/playground/kube/mifos.tf
+++ live/playground/kube/mifos.tf
@@ -1,10 +1,46 @@
 # Exposed services for Mifos
+
+resource "aws_lb_target_group" "mifos_bb_api" {
+  name = "mifos-bb-api-tg"
+  port = "5000"
+  vpc_id = var.vpc_id
+  protocol = "HTTP"
+  target_type = "ip"
+
+  health_check {
+    protocol = "HTTP"
+    path = "/actuator/health"
+    port = "5000"
+  }
+}
+
 resource "aws_lb_target_group" "mifos_bb_ui" {
   name        = "mifos-bb-ui-tg"
   port        = 80
   vpc_id      = var.vpc_id
   protocol    = "HTTP"
   target_type = "ip"
+}
+
+resource "kubernetes_manifest" "mifos_bb_api" {
+  lifecycle {
+    replace_triggered_by = [aws_lb_target_group.mifos_bb_api]
+  }
+  manifest = {
+    apiVersion = "elbv2.k8s.aws/v1beta1"
+    kind       = "TargetGroupBinding"
+    metadata = {
+      name      = aws_lb_target_group.mifos_bb_api.name
+      namespace = "paymenthub"
+    }
+    spec = {
+      serviceRef = {
+        name = "ph-ee-operations-app"
+        port = 5000
+      }
+      targetGroupARN = aws_lb_target_group.mifos_bb_api.arn
+    }
+  }
 }
 
 resource "kubernetes_manifest" "mifos_bb_ui" {
@@ -57,6 +93,45 @@ resource "aws_lb_listener_rule" "mifos_bb_admin" {
   condition {
     host_header {
       values = ["admin-dashboard-mifos-bb.${var.alb_domain}"]
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "mifos_bb_admin_api" {
+  lifecycle {
+    replace_triggered_by = [aws_lb_target_group.mifos_bb_api]
+  }
+  listener_arn = var.sandbox_alb_listener_arn
+  priority     = 33053
+  tags = {
+    Name = "mifos-bb admin dashboard"
+  }
+
+  action {
+    type             = "authenticate-cognito"
+    authenticate_cognito {
+      user_pool_arn = "${var.user_pool_arn}"
+      user_pool_client_id = "${var.user_pool_client_id}"
+      user_pool_domain = "${var.user_pool_domain}"
+      session_timeout = "7200"
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.mifos_bb_api.arn
+  }
+
+  condition {
+    host_header {
+      values = ["admin-dashboard-mifos-bb.${var.alb_domain}"]
+    }
+  }
+  condition {
+    path_pattern {
+      values = [
+        "/v1/*",
+      ]
     }
   }
 }
