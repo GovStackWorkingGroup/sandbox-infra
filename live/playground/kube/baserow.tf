da1ec2ec55592ba7d727382f2884fee280683f8a
oleksiidn <oleksii.danyliuk@gofore.com>
2024-09-03 13:45:50 +0300

    in progress

--- live/playground/kube/baserow.tf
+++ live/playground/kube/baserow.tf
@@ -7,6 +7,14 @@ resource "aws_lb_target_group" "baserow_bb_ui" {
   target_type = "ip"
 }
 
+resource "aws_lb_target_group" "baserow_backend" {
+  name        = "baserow-backend-tg"
+  port        = 8000
+  vpc_id      = var.vpc_id
+  protocol    = "HTTP"
+  target_type = "ip"
+}
+
 resource "kubernetes_manifest" "baserow_bb_ui" {
   lifecycle {
     replace_triggered_by = [aws_lb_target_group.baserow_bb_ui]
@@ -24,6 +32,27 @@ resource "kubernetes_manifest" "baserow_bb_ui" {
         port = 3000
       }
       targetGroupARN = aws_lb_target_group.baserow_bb_ui.arn
+    }
+  }
+}
+
+resource "kubernetes_manifest" "baserow_backend" {
+  lifecycle {
+    replace_triggered_by = [aws_lb_target_group.baserow_backend]
+  }
+  manifest = {
+    apiVersion = "elbv2.k8s.aws/v1beta1"
+    kind       = "TargetGroupBinding"
+    metadata = {
+      name      = aws_lb_target_group.baserow_backend.name
+      namespace = "baserow"
+    }
+    spec = {
+      serviceRef = {
+        name = "baserow-wsgi"
+        port = 8000
+      }
+      targetGroupARN = aws_lb_target_group.baserow_backend.arn
     }
   }
 }
@@ -52,6 +81,37 @@ resource "aws_lb_listener_rule" "baserow_bb" {
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.baserow_bb_ui.arn
+  }
+
+  condition {
+    host_header {
+      values = ["baserow-bb.${var.alb_domain}"]
+    }
+  }
+}
+resource "aws_lb_listener_rule" "baserow_backend" {
+  lifecycle {
+    replace_triggered_by = [aws_lb_target_group.baserow_backend]
+  }
+  listener_arn = var.sandbox_alb_listener_arn
+  priority     = 3406
+  tags = {
+    Name = "baserow-wsgi"
+  }
+
+  action {
+    type             = "authenticate-cognito"
+    authenticate_cognito {
+      user_pool_arn = "${var.user_pool_arn}"
+      user_pool_client_id = "${var.user_pool_client_id}"
+      user_pool_domain = "${var.user_pool_domain}"
+      session_timeout = "7200"
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.baserow_backend.arn
   }
 
   condition {
