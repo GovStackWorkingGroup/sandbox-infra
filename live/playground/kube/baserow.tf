c92130c7c1812aad3eafb03df85de27c39790978
oleksiidn <oleksii.danyliuk@gofore.com>
2024-09-03 20:58:23 +0300

    refactoring

--- live/playground/kube/baserow.tf
+++ live/playground/kube/baserow.tf
@@ -1,12 +1,4 @@
 # Exposed services for baserow
-resource "aws_lb_target_group" "baserow_bb_ui" {
-  name        = "baserow-bb-ui-tg"
-  port        = 80
-  vpc_id      = var.vpc_id
-  protocol    = "HTTP"
-  target_type = "ip"
-}
-
 resource "aws_lb_target_group" "baserow_backend" {
   name        = "baserow-backend-tg"
   port        = 8000
@@ -15,72 +7,62 @@ resource "aws_lb_target_group" "baserow_backend" {
   target_type = "ip"
 }
 
-resource "kubernetes_manifest" "baserow_bb_ui" {
-  lifecycle {
-    replace_triggered_by = [aws_lb_target_group.baserow_bb_ui]
-  }
-  manifest = {
-    apiVersion = "elbv2.k8s.aws/v1beta1"
-    kind       = "TargetGroupBinding"
-    metadata = {
-      name      = aws_lb_target_group.baserow_bb_ui.name
+resource "aws_lb_target_group" "baserow_bb_ui" {
+  name = "baserow-bb-ui-tg"
+  port = 80
+  vpc_id = var.vpc_id
+  protocol = "HTTP"
+  target_type = "ip"
+}
+
+locals {
+  baserow_backends = {
+    baserow_bb_ui = {
       namespace = "baserow"
+      service = "baserow-frontend"
+      tg = aws_lb_target_group.baserow_bb_ui
     }
-    spec = {
-      serviceRef = {
-        name = "baserow-frontend"
-        port = 3000
-      }
-      targetGroupARN = aws_lb_target_group.baserow_bb_ui.arn
+    baserow_backend = {
+      namespace = "baserow"
+      service = "baserow-wsgi"
+      tg = aws_lb_target_group.baserow_backend
     }
   }
 }
 
-resource "kubernetes_manifest" "baserow_backend" {
-  lifecycle {
-    replace_triggered_by = [aws_lb_target_group.baserow_backend]
-  }
+resource "kubernetes_manifest" "baserow_backends" {
+  for_each = local.baserow_backends
   manifest = {
     apiVersion = "elbv2.k8s.aws/v1beta1"
-    kind       = "TargetGroupBinding"
+    kind = "TargetGroupBinding"
     metadata = {
-      name      = aws_lb_target_group.baserow_backend.name
-      namespace = "baserow"
+      name = "${each.value.service}-tg"
+      namespace = "${each.value.namespace}"
     }
     spec = {
       serviceRef = {
-        name = "baserow-wsgi"
-        port = 8000
+        name = "${each.value.service}"
+        port = "${each.value.tg.port}"
       }
-      targetGroupARN = aws_lb_target_group.baserow_backend.arn
+      targetGroupARN = "${each.value.tg.arn}"
     }
   }
 }
-
-
-resource "aws_lb_listener_rule" "baserow_bb" {
-  lifecycle {
-    replace_triggered_by = [aws_lb_target_group.baserow_bb_ui]
-  }
+resource "aws_lb_listener_rule" "baserow_backend" {
   listener_arn = var.sandbox_alb_listener_arn
-  priority     = 3405
-  tags = {
-    Name = "baserow-bb main ui"
-  }
-
-  action {
-    type             = "authenticate-cognito"
-    authenticate_cognito {
-      user_pool_arn = "${var.user_pool_arn}"
-      user_pool_client_id = "${var.user_pool_client_id}"
-      user_pool_domain = "${var.user_pool_domain}"
-      session_timeout = "7200"
-    }
-  }
+  priority     = 3406
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.baserow_bb_ui.arn
+    target_group_arn = aws_lb_target_group.baserow_backend.arn
+  }
+
+  condition {
+    path_pattern {
+      values = [
+        "/api/*"
+      ]
+    }
   }
 
   condition {
@@ -89,29 +71,14 @@ resource "aws_lb_listener_rule" "baserow_bb" {
     }
   }
 }
-resource "aws_lb_listener_rule" "baserow_backend" {
-  lifecycle {
-    replace_triggered_by = [aws_lb_target_group.baserow_backend]
-  }
-  listener_arn = var.sandbox_alb_listener_arn
-  priority     = 3406
-  tags = {
-    Name = "baserow-wsgi"
-  }
 
-  action {
-    type             = "authenticate-cognito"
-    authenticate_cognito {
-      user_pool_arn = "${var.user_pool_arn}"
-      user_pool_client_id = "${var.user_pool_client_id}"
-      user_pool_domain = "${var.user_pool_domain}"
-      session_timeout = "7200"
-    }
-  }
+resource "aws_lb_listener_rule" "baserow_bb_ui" {
+  listener_arn = var.sandbox_alb_listener_arn
+  priority     = 3405
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.baserow_backend.arn
+    target_group_arn = aws_lb_target_group.baserow_bb_ui.arn
   }
 
   condition {
