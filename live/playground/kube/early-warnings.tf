c60903c426aa8a4116f1d51bd288ffe0b96f7613
oleksii <oleksiidn@proton.me>
2024-11-25 14:09:54 +0200

    draft update

--- live/playground/kube/early-warnings.tf
+++ live/playground/kube/early-warnings.tf
@@ -1,52 +1,87 @@
-# Exposed services for joget
-resource "aws_lb_target_group" "early_warnings_ui" {
-  name        = "early-warnings-ui-tg"
+resource "aws_lb_target_group" "early_warnings_backend" {
+    name = "early-warnings-backend-tg"
+    port = 8080
+    vpc_id = var.vpc_id
+    protocol = "HTTP"
+    target_type = "ip"
+}
+resource "aws_lb_target_group" "early_warnings_fronted" {
+  name        = "early-warnings-frontend-tg"
   port        = 80
   vpc_id      = var.vpc_id
   protocol    = "HTTP"
   target_type = "ip"
 }
 
-resource "kubernetes_manifest" "early_warnings_ui" {
-  lifecycle {
-    replace_triggered_by = [aws_lb_target_group.early_warnings_ui]
-  }
-  manifest = {
-    apiVersion = "elbv2.k8s.aws/v1beta1"
-    kind       = "TargetGroupBinding"
-    metadata = {
-      name      = aws_lb_target_group.early_warnings_ui.name
+locals {
+  early_warnings = {
+    early_warnings_fronted = {
       namespace = "early-warnings"
+      service = "frontend-service"
+      tg = aws_lb_target_group.early_warnings_fronted
     }
-    spec = {
-      serviceRef = {
-        name = "user-service"
-        port = 8080
-      }
-      targetGroupARN = aws_lb_target_group.early_warnings_ui.arn
+    usct_backend = {
+      namespace = "early-warnings"
+      service = "threat-service"
+      tg = aws_lb_target_group.early_warnings_backend
     }
   }
 }
 
+resource "kubernetes_manifest" "early-warnings" {
+  for_each = local.early_warnings
+  manifest = {
+    apiVersion = "elbv2.k8s.aws/v1beta1"
+    kind = "TargetGroupBinding"
+    metadata = {
+      name = "${each.value.service}-tg"
+      namespace = "${each.value.namespace}"
+    }
+    spec = {
+      serviceRef = {
+        name = "${each.value.service}"
+        port = "${each.value.tg.port}"
+      }
+      targetGroupARN = "${each.value.tg.arn}"
+    }
+  }
+}
 
-resource "aws_lb_listener_rule" "early_warnings_bb" {
-  lifecycle {
-    replace_triggered_by = [aws_lb_target_group.early_warnings_ui]
-  }
+resource "aws_lb_listener_rule" "early_warnings_backend" {
+
   listener_arn = var.sandbox_alb_listener_arn
-  priority     = 3405
-  tags = {
-    Name = "joget-bb main ui"
-  }
+  priority     = 3415
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.early_warnings_ui.arn
+    target_group_arn = aws_lb_target_group.early_warnings_backend.arn
+  }
+    condition {
+      path_pattern {
+        values = [
+          "/api/*"
+        ]
+      }
+    }
+
+  condition {
+    host_header {
+      values = ["early-warnings.${var.alb_domain}"]
+    }
+  }
+}
+resource "aws_lb_listener_rule" "early_warnings_fronted" {
+  listener_arn = var.sandbox_alb_listener_arn
+  priority     = 3416
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.early_warnings_fronted.arn
   }
 
   condition {
     host_header {
-      values = ["joget-bb.${var.alb_domain}"]
+      values = ["early-warnings.${var.alb_domain}"]
     }
   }
 }
