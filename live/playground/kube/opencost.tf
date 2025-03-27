ac46468de1747e1e02d57beac019621ab95c9231
Oleksii Danyliuk <oleksii.danyliuk@gofore.com>
2025-03-27 09:23:42 +0000

    Add Opencost BB
    * add opencost UI
    
    * remove unused BB - rapidpro.
    
    ---------
    
    Co-authored-by: oleksiidn <oleksiidn@proton.me>

--- live/playground/kube/opencost.tf
+++ live/playground/kube/opencost.tf
@@ -1,42 +1,42 @@
-# Exposed services for RapidPro
-resource "aws_lb_target_group" "rapidpro_bb_ui" {
-  name        = "rapidpro-bb-ui-tg"
+# Exposed services for OpenCost
+resource "aws_lb_target_group" "opencost_bb_ui" {
+  name        = "opencost-bb-ui-tg"
   port        = 80
   vpc_id      = var.vpc_id
   protocol    = "HTTP"
   target_type = "ip"
 }
 
-resource "kubernetes_manifest" "rapidpro_bb_ui" {
+resource "kubernetes_manifest" "opencost_bb_ui" {
   lifecycle {
-    replace_triggered_by = [aws_lb_target_group.rapidpro_bb_ui]
+    replace_triggered_by = [aws_lb_target_group.opencost_bb_ui]
   }
   manifest = {
     apiVersion = "elbv2.k8s.aws/v1beta1"
     kind       = "TargetGroupBinding"
     metadata = {
-      name      = aws_lb_target_group.rapidpro_bb_ui.name
-      namespace = "rapidpro"
+      name      = aws_lb_target_group.opencost_bb_ui.name
+      namespace = "opencost"
     }
     spec = {
       serviceRef = {
-        name = "rapidpro"
-        port = 8000
+        name = "opencost"
+        port = 9090
       }
-      targetGroupARN = aws_lb_target_group.rapidpro_bb_ui.arn
+      targetGroupARN = aws_lb_target_group.opencost_bb_ui.arn
     }
   }
 }
 
 
-resource "aws_lb_listener_rule" "rapidpro_bb" {
+resource "aws_lb_listener_rule" "opencost_bb_admin" {
   lifecycle {
-    replace_triggered_by = [aws_lb_target_group.rapidpro_bb_ui]
+    replace_triggered_by = [aws_lb_target_group.opencost_bb_ui]
   }
   listener_arn = var.sandbox_alb_listener_arn
-  priority     = 3403
+  priority     = 3405
   tags = {
-    Name = "rapidpro-bb main ui"
+    Name = "opencost-bb admin dashboard"
   }
 
   action {
@@ -51,12 +51,12 @@ resource "aws_lb_listener_rule" "rapidpro_bb" {
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.rapidpro_bb_ui.arn
+    target_group_arn = aws_lb_target_group.opencost_bb_ui.arn
   }
 
   condition {
     host_header {
-      values = ["rapidpro-bb.${var.alb_domain}"]
+      values = ["admin-dashboard-opencost.${var.alb_domain}"]
     }
   }
 }
